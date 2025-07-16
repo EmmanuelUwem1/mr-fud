66522730732ad63f8ab48aec7e56109a8ef94073
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-07-16 22:08:12 +0100

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900", 
-});
 
 export const metadata: Metadata = {
   title: "FUDClub",
@@ -20,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={geistSans.variable}>
+    <html lang="en">
       <head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
