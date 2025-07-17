cf5a1926275e0646648c9dc4f13ebd2f836f3130
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-07-17 21:34:56 +0100

    add leaderboard page

---
+++ src/app/leaderboard/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+function Page() {
+  return (
+    <div className="flex w-full items-start flex-col justify-start">
+      Leaderboard page
+    </div>
+  )
+}
+
+export default Page
