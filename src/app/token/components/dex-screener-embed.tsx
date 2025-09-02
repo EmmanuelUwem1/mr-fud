dd2f87af48e857481c5349bfc925a094d81f8f92
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-09-02 16:39:16 +0100

    update: Ocicat chart

--- src/app/token/components/dex-screener-embed.tsx
+++ src/app/token/components/dex-screener-embed.tsx
@@ -7,11 +7,12 @@ const DexScreenerEmbed = () => {
       className="w-full h-full"
       id="dexscreener-embed"
       title="DEX Screener Embed"
-      src="https://dexscreener.com/bsc/0x1df65d3a75AeCd000A9c17c97E99993aF01DbcD1?embed=1&loadChartSettings=0&trades=0&tabs=0&info=0&chartLeftToolbar=0&chartDefaultOnMobile=1&chartTheme=dark&theme=dark&chartStyle=0&chartType=usd&interval=15"
+      src="https://dexscreener.com/bsc/0x1df65d3a75AeCd000A9c17c97E99993aF01DbcD1?embed=1&loadChartSettings=0&trades=0&tabs=0&info=0&chartLeftToolbar=0&chartDefaultOnMobile=1&chartTheme=dark&theme=dark&chartStyle=1&chartType=usd&interval=15"
       frameBorder="0"
       allow="clipboard-write"
       allowFullScreen
     ></iframe>
+
   );
 };
 
