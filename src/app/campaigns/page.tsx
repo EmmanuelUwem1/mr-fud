f5dc977ec9bf2429d1cb17a5992bc13b748f163b
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-09-08 22:20:20 +0100

    update

--- src/app/campaigns/page.tsx
+++ src/app/campaigns/page.tsx
@@ -2,21 +2,23 @@
 import Banner from "./components/campaignsBanner";
 import { motion } from "framer-motion";
 import CampaignsSection from "./components/campaigns-section";
+import { CampaignProvider } from "@/context/campaignsContext";
+
 
 
 function Page() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-      className="flex w-full items-start flex-col justify-start"
-    >
-     
-
-      <Banner />
-      <CampaignsSection />
-    </motion.div>
+    <CampaignProvider>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        className="flex w-full items-start flex-col justify-start"
+      >
+        <Banner />
+        <CampaignsSection />
+      </motion.div>
+    </CampaignProvider>
   );
 }
 
